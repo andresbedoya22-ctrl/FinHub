a9656c91556ae2e48f9518420f59aa6fbaa78627
Andres Bedoya <andres.bedoya22@gmail.com>
2025-12-27 14:36:38 +0100

    chore(next): migrate middleware to proxy convention

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
-﻿import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { updateSession } from "./src/lib/supabase/middleware";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
   const pathname = request.nextUrl.pathname;
 
   const response = await updateSession(request);
