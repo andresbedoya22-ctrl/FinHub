a7a0164a813b43c2945aea3a1145ff213f17bb1b
Andres Bedoya <andres.bedoya22@gmail.com>
2025-12-31 21:24:10 +0100

    chore(i18n): wire next-intl provider in root layout (F01.6)

---
+++ src/i18n/request.ts
@@ -0,0 +1,8 @@
+﻿import { getLocale } from "./getLocale";
+import { getMessages } from "./getMessages";
+
+export async function getI18nRequestContext() {
+  const locale = await getLocale();
+  const messages = await getMessages(locale);
+  return { locale, messages };
+}
